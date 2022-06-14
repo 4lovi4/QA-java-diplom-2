292da4e147dcaee350d29825f50cb5250175b496
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2022-06-15 01:41:36 +0300

    initial commit

---
+++ src/main/java/client/BaseRestClient.java
@@ -0,0 +1,13 @@
+package client;
+
+import io.restassured.config.RedirectConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.config.SSLConfig;
+import io.restassured.response.Response;
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class BaseRestClient {
+
+}
